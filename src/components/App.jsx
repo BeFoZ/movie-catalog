9918c89f6a387c15a294cb580b4b64a05044e672
BeFoZ <yuriiv77751@gmail.com>
2025-04-18 19:35:44 +0300

    feat(navbar): add navigation bar with dropdown menu and search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Layout from './Layout';
-import Movies from './pages/Movies/Movies';
-import MovieDetails from './pages/MovieDetails/MovieDetails';
+import Layout from "./Layout";
+import Movies from "./pages/Movies/Movies";
+import MovieDetails from "./pages/MovieDetails/MovieDetails";
+import MoviesListTop from "./pages/MoviesListTop/MoviesListTop";
+import MoviesListMain from "./pages/MoviesListMain/MoviesListMain";
+import { MOVIE_LISTS, TOP_LISTS } from './constants';
 
 function App() {
   const router = createBrowserRouter([
@@ -16,17 +16,25 @@ function App() {
       children: [
         {
           path: "/",
-          element: <Movies />
+          element: <Movies />,
         },
+        ...TOP_LISTS.map(el => ({
+          path: el.url,
+          element: <MoviesListTop />,
+        })),
+        ...MOVIE_LISTS.map(el => ({
+          path: el.url,
+          element: <MoviesListMain />,
+        })),
         {
           path: "/movie/:id",
-          element: <MovieDetails />
-        } 
-      ]
+          element: <MovieDetails />,
+        },
+      ],
     },
   ]);
 
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
