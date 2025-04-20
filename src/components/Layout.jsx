3b258d69934b64333b5329594684d0892de552f3
DenisSumDU <ivashchenko.denys@student.sumdu.edu.ua>
2025-04-20 08:44:27 +0300

    adapt layout for footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,32 @@
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 
 export default function Layout() {
   return (
-    <Container fixed>
-      <Box sx={{ p: 4 }} />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Navbar />
-      <Outlet />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 4,
+          maxWidth: 1200,
+          margin: "0 auto",
+          width: "100%",
+        }}
+      >
+        <Outlet />
+      </Box>
       <Footer />
-    </Container>
+    </Box>
   );
 }
