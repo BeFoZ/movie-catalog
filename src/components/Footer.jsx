debe474efd8a388e9bf9890a5e9ffe83b759016a
DenisSumDU <ivashchenko.denys@student.sumdu.edu.ua>
2025-04-20 08:44:49 +0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { AppBar, Container, Toolbar, Typography, Stack } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <AppBar
+      position="static"
+      component="footer"
+      sx={{
+        top: "auto",
+        backgroundColor: "background.paper",
+        color: "text.primary",
+        mt: "auto",
+        py: 1,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            width="100%"
+          >
+            <Typography variant="body2">
+              Copyright © {new Date().getFullYear()} Team 8. All rights
+              reserved.
+            </Typography>
+
+            <Typography
+              component={RouterLink}
+              to="/"
+              variant="h6"
+              sx={{
+                textDecoration: "none",
+                color: "inherit",
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+              }}
+            >
+              CINEPOST
+            </Typography>
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
