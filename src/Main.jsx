f6c47b2aa24e3809e79cdc6ea719a8b00e63c47e
Kyrylo Kolesnichenko <kykocq@gmail.com>
2025-04-22 23:15:07 +0300

    installed and connected redux toolkit

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,15 @@
 import { CssBaseline } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css';
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store.js'
 
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <Provider store={store}>
     <CssBaseline /> 
     <App />
-  </>
+  </Provider>
 )
