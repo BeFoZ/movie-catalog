930096ecd8c587518cf7b46b5c7f81173bb9fe4a
RillSvai <kirilkvas07@gmail.com>
2025-04-25 11:23:43 +0300

    import movies from local database

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import MovieDetails from "./pages/MovieDetails.jsx";
 import Favorites from "./pages/Favorites.jsx";
+import moviesData from "../movies.json";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,7 +13,7 @@ function App() {
       path: "/",
       element: <Layout />,
       children: [
-        { path: "/", element: <Home /> },
+        { path: "/", element: <Home movies={moviesData} /> },
         { path: "/favorites", element: <Favorites /> },
         { path: "/movie/:id", element: <MovieDetails /> },
       ],
